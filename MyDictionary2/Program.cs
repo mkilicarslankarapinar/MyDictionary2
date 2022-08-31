244d67aadf864e6096cb8fb2a304d23dab99fa03
mkilicarslan <mkilicarslankarapinar@gmail.com>
2022-08-31 13:42:42 +0300

    Add project files.

---
+++ MyDictionary2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler1 = new List<string>();
+            sehirler1.Add("Kastamonu");
+            Console.WriteLine(sehirler1.Count);
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Bayburt");
+            sehirler2.Add("Bayburt");
+            sehirler2.Add("Bayburt");
+            sehirler2.Add("Bayburt");
+            sehirler2.Add("Bayburt");
+            Console.WriteLine(sehirler2.Count);
+
+        }
+    }
+}
